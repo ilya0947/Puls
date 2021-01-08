245205b91b49d51dbd43a50aac3b7eb5bab0c59d
Илья <m.i.v.89@mail.ru>
2021-01-09 03:59:42 +0500

    Создание табов

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 const slider = tns({
     container: '.carousel__inner',
     items: 1,
+    speed: 2000,
     slideBy: 'page',
     autoplay: false,
     adaptiveHeight: true,
@@ -27,4 +28,24 @@ const slider = tns({
   });
   document.querySelector('.slick-next').addEventListener('click', function () {
     slider.goTo('next');
+  });
+
+  $(document).ready (function(){
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+      $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function tooggleSlide(item){
+      $(item).each(function(i){
+        $(this).on('click', function(e){
+          e.preventDefault();
+          $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+          $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        })
+      })
+    };
+    tooggleSlide('.catalog-item__link');
+    tooggleSlide('.catalog-item__back')
   });
